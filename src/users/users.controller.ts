1ccf7ec64c5bb52611edb74df19e75c62113cd39
mashood <mashood@qashio.com>
2024-02-09 19:12:08 +0400

    current user decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { currentUser } from 'src/auth/decorator';
 
 @Controller('users')
 export class UsersController {
 
   @UseGuards(AuthGuard('jwt-auth'))
   @Get('me')
-  getMe(@Req() req: Request) {
+  getMe(@currentUser() currentUser: User) {
     // console.log({ user: req.user }, 'user.....')
-    return req.user
+    return currentUser
   }
 }
